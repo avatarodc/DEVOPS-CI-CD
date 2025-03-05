53f4ab41d22f51a92c7c3bfda6be2e4411d4c1a8
avatarodc <odcmamadougueye@gmail.com>
2025-03-05 23:04:33 +0000

    feat: ajout des tests IHM avec Cypress

---
+++ frontend/cypress.config.js
@@ -0,0 +1,11 @@
+// Fichier: frontend/cypress.config.js
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost:3000',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+})
